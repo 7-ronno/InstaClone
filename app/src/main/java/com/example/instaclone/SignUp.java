9a987eff1b702ebdf2fb18dc6105284b68e5bf5b
7-ronno <maanas_b180361cs@nitc.ac.in>
2020-04-13 11:29:51 +0530

    Fancy Toast+Error try and catch

--- app/src/main/java/com/example/instaclone/SignUp.java
+++ app/src/main/java/com/example/instaclone/SignUp.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +11,7 @@ import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
@@ -34,23 +34,26 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        final ParseObject boxer=new ParseObject("Boxers");
-        boxer.put("name",name.getText().toString());
-        boxer.put("power",Integer.parseInt(power.getText().toString()));
-        boxer.put("speed",Integer.parseInt(speed.getText().toString()));
+        try {
+            final ParseObject boxer = new ParseObject("Boxers");
+            boxer.put("name", name.getText().toString());
+            boxer.put("power", Integer.parseInt(power.getText().toString()));
+            boxer.put("speed", Integer.parseInt(speed.getText().toString()));
 
-        boxer.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if(e==null)
-                {
-                    Toast.makeText(SignUp.this,boxer.get("name")+" has been saved to server",Toast.LENGTH_LONG).show();
+            boxer.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        FancyToast.makeText(SignUp.this, boxer.get("name") + " has been saved to server", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                    } else {
+                        FancyToast.makeText(SignUp.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                    }
                 }
-                else
-                {
-                    Toast.makeText(SignUp.this,e.getMessage(),Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+            });
+        }
+        catch (Exception e)
+        {
+            FancyToast.makeText(SignUp.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+        }
     }
 }
